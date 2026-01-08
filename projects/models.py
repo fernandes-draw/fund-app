34c2bb4eb807be01a7c237c6b1b103b18f9aef4c
fernandes-draw <fernandes_draw@hotmail.com>
2026-01-08 07:14:32 -0300

    Criado a models para Projetos, executado o comando makemigrations e migrate

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Project(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    code = models.CharField(max_length=15)
+    description = models.CharField(max_length=255)
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.code
