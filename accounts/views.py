82fef76ccc6d341bc7798ddbc3b180b1baf43df8
fernandes-draw <fernandes_draw@hotmail.com>
2026-01-09 12:10:40 -0300

    Finalizando últimas tarefas

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import View
 from projects.models import Project
+from tasks.models import Task
 
 
 class DashboardView(View):
     def get(self, request, *args, **kwargs):
         latest_projects = Project.objects.all()[:5]
+        latest_tasks = Task.objects.all()[:5]
 
         context = {}
         context["latest_projects"] = latest_projects
+        context["latest_tasks"] = latest_tasks
+        
         return render(request, "accounts/dashboard.html", context)
