3da08c0511f6dc6e1ecda0432cba5983428b26e0
Everson Fernandes <fernandes_draw@hotmail.com>
2026-01-09 21:18:43 -0300

    Adicionado propriedades de perfil do usuário

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 # import django signals
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils.timesince import timesince
+from django.utils import timezone
+from datetime import timedelta
 
 
 class Profile(models.Model):
@@ -17,11 +20,46 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
+    @property
+    def profile_picture_url(self):
+        try:
+            image = self.profile_picture.url
+        except:
+            image = "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909__340.png"
+        return image
 
-# @receiver(post_save, sender=User)
-# def create_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
+    # @property
+    # def full_name(self):
+    #     first_name = self.user.first_name
+    #     last_name = self.user.last_name
+
+    #     if first_name and last_name:
+    #         return f"{first_name} {last_name}"
+
+    #     return self.user.username.capitalize
+
+    @property
+    def full_name(self):
+        name = self.user.get_full_name()
+
+        if name:
+            return name
+
+        return self.user.get_username().capitalize
+
+    # @property
+    # def date_joined(self):
+    #     return timesince(self.user.date_joined)
+
+    @property
+    def date_joined(self):
+        time_diff = timezone.now() - self.user.date_joined
+
+        if time_diff <= timedelta(days=2):
+            return timesince(self.user.date_joined) + " atrás"
+
+        else:
+            return self.user.date_joined.strftime("%d %b")
 
 
 @receiver(post_save, sender=User)
